dd435386a2559614f8191c516a95469b46aa64b5
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2024-07-18 08:59:34 -0300

    criando entidade User e migrations

---
+++ src/CashFlow.Domain/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace CashFlow.Domain.Entities;
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public Guid UserIdentifier { get; set; }
+    public string Role { get; set; } = string.Empty;
+}
