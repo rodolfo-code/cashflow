1cbd98e87bdde0a5526ad6fb62420e311cafd87f
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2024-07-22 09:01:51 -0300

    iniciando testes de usuario

--- tests/Validators.Tests/obj/Debug/net8.0/Validators.Tests.AssemblyInfo.cs
+++ tests/Validators.Tests/obj/Debug/net8.0/Validators.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Validators.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9b97489d69caebe8bc66aa72ceb3933d1c7727f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09657a4221ddb541941d46130ffd64ba516f3564")]
 [assembly: System.Reflection.AssemblyProductAttribute("Validators.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Validators.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
