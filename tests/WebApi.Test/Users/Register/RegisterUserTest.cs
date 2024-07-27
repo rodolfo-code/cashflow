dc2181f459e3bfcc1f36a8b6e1ba77d45390d29d
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2024-07-27 10:41:54 -0300

    testando integracao de criacao de usuario com nome invalido

--- tests/WebApi.Test/Users/Register/RegisterUserTest.cs
+++ tests/WebApi.Test/Users/Register/RegisterUserTest.cs
@@ -1,7 +1,9 @@
-﻿using CommonTestUtilities.Requests;
+﻿using CashFlow.Exception;
+using CommonTestUtilities.Requests;
 using FluentAssertions;
-using Microsoft.AspNetCore.Mvc.Testing;
+using System.Globalization;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text.Json;
 
@@ -31,5 +33,37 @@ public class RegisterUserTest : IClassFixture<CustomWebApplicationFactory>
 
         response.RootElement.GetProperty("name").GetString().Should().Be(request.Name);
         response.RootElement.GetProperty("token").GetString().Should().NotBeNullOrEmpty();
+    }
+
+    [Theory(DisplayName = nameof(Register_User_Exception_When_Name_empty))]
+    [Trait("Integration", "Register User")]
+    [InlineData("en")]
+    [InlineData("pt-BR")]
+    public async Task Register_User_Exception_When_Name_empty(string cultureInfo)
+    {
+        // Arrange
+        var request = RequestRegisterUserJsonBuilder.Build();
+        request.Name = string.Empty;
+        _httpClient.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue(cultureInfo));
+
+        // Act
+        HttpResponseMessage result = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+        // Assert
+        result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        var body = await result.Content.ReadAsStreamAsync();
+
+        var response = await JsonDocument.ParseAsync(body);
+
+        var errors = response.RootElement.GetProperty("errorMessages").EnumerateArray();
+
+        var expectedMessage = ResourceErrorMessages.ResourceManager.GetString("NAME_EMPTY", new CultureInfo(cultureInfo));
+
+        errors.Should().HaveCount(1);
+        errors.First().ToString().Should().Be(expectedMessage);
+
+        // OU
+
+        //errors.Should().HaveCount(1).And.Contain(error => error.GetString()!.Equals(ResourceErrorMessages.NAME_EMPTY));
     }
 }
