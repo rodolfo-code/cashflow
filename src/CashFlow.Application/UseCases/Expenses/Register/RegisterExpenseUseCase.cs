97353bfa6276bc860745aaaa1e27261e24313434
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2024-08-02 22:14:59 -0300

    incluindo usuario na criacao da despesa

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,10 +1,10 @@
 ﻿using AutoMapper;
-using CashFlow.Communication.Enums;
 using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 using CashFlow.Domain.Entities;
 using CashFlow.Domain.Repositories;
 using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Domain.Services.LoggedUser;
 using CashFlow.Exception.ExceptionBase;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
@@ -13,24 +13,29 @@ public class RegisterExpenseUseCase : IRegisterExpenseUseCase
     private readonly IExpensesWriteOnlyRepository _repository;
     private readonly IUnitOfWork _unitOfWork;
     private readonly IMapper _mapper;
-    public RegisterExpenseUseCase(IExpensesWriteOnlyRepository repository, IUnitOfWork unitOfWork, IMapper mapper)
+    private readonly ILoggedUser _loggedUser;
+    public RegisterExpenseUseCase(IExpensesWriteOnlyRepository repository, IUnitOfWork unitOfWork, IMapper mapper, ILoggedUser loggedUser)
     {
         _repository = repository;
         _unitOfWork = unitOfWork;
         _mapper = mapper;
+        _loggedUser = loggedUser;
     }
 
     public async Task<ResponseRegisteredExpenseJson> Execute(RequestExpenseJson request)
     {
         validate(request);
 
-        var entity = _mapper.Map<Expense>(request);
+        var loggedUser = await _loggedUser.Get();
 
-        await _repository.Add(entity);
+        var expense = _mapper.Map<Expense>(request);
+        expense.UserId = loggedUser.Id;
+
+        await _repository.Add(expense);
 
         await _unitOfWork.Commit();
 
-        return _mapper.Map<ResponseRegisteredExpenseJson>(entity);
+        return _mapper.Map<ResponseRegisteredExpenseJson>(expense);
     }
 
 
